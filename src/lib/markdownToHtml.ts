0a558369f230f51c6eb77d6b87ff73f3f0e33c08
David Stotijn <dstotijn@gmail.com>
2021-12-31 15:43:37 +0100

    Update deps

--- src/lib/markdownToHtml.ts
+++ src/lib/markdownToHtml.ts
@@ -1,10 +1,12 @@
-import remark from "remark";
+import { remark } from "remark";
 import smartypants from "@ngsctt/remark-smartypants";
 import html from "remark-html";
 
 export default async function markdownToHtml(
   markdown: string
 ): Promise<string> {
-  const result = await remark().use(smartypants).use(html).process(markdown);
+  const result = await remark().use(smartypants).use(html, {
+    sanitize: false,
+  }).process(markdown);
   return result.toString();
 }
